7c5dd70ef46c774fc1b3b233610d1b8d13c97443
andamagodwin <andamagodwinezra@gmail.com>
2025-01-03 22:21:29 +0300

    Added the login and the register screens

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,9 +1,31 @@
 import React, { useState } from 'react'
-import { Alert, StyleSheet, View, AppState } from 'react-native'
+import { Alert, StyleSheet, View } from 'react-native'
 import { supabase } from '@/lib/supabase'
 import { Button, Input } from '@rneui/themed'
+import { Text } from 'react-native'
+import { Link } from 'expo-router'
 
 export default function Login (){
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [loading, setLoading] = useState(false)
+
+
+    async function signInWithEmail() {
+        setLoading(true)
+        const { error } = await supabase.auth.signInWithPassword({
+          email: email,
+          password: password,
+        })
+    
+        if (error) Alert.alert(error.message)
+        setLoading(false)
+      }
+    
+      
+
+
     return (
         <View style={styles.container}>
           <View style={[styles.verticallySpaced, styles.mt20]}>
@@ -31,9 +53,25 @@ export default function Login (){
             <Button title="Sign in" disabled={loading} onPress={() => signInWithEmail()} />
           </View>
           <View style={styles.verticallySpaced}>
-            <Button title="Sign up" disabled={loading} onPress={() => signUpWithEmail()} />
+            <Text>Don't have an account? </Text>
+            <Link href="/register">register</Link>
           </View>
-         
         </View>
       )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    padding: 12,
+  },
+  verticallySpaced: {
+    paddingTop: 4,
+    paddingBottom: 4,
+    alignSelf: 'stretch',
+  },
+  mt20: {
+    marginTop: 20,
+  },
+})
